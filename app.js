adb02ea895faba893400fe0f3aee0fafef5b25b3
Astrid Corena <astrid.corena@gmail.com>
2019-05-24 14:38:52 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express();
+
+app.get('/',(req, res) => {
+  res.send(req.headers['user-agent'])
+});
+app.listen(3000, () => console.log('listening 3000'));
